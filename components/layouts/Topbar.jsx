de7bc1abf8cf3923b7a2c83689efaf49c989c742
mimamch <mimamch28@gmail.com>
2022-09-19 13:38:12 +0700

    clean header

--- components/layouts/Topbar.jsx
+++ components/layouts/Topbar.jsx
@@ -10,22 +10,22 @@ export default function Topbar() {
           <div className="navbar-brand-box">
             <Link href="/">
               <a className="logo logo-dark ">
-                <span className="logo-sm">
+                {/* <span className="logo-sm">
                   <img src="/assets/images/logo.svg" alt="" height={22} />
-                </span>
+                </span> */}
                 <span className="logo-lg">
                   <img src="/assets/images/logo-dark.png" alt="" height={17} />
                 </span>
               </a>
             </Link>
-            <a href="index.html" className="logo logo-light">
+            {/* <a href="/" className="logo logo-light">
               <span className="logo-sm">
                 <img src="/assets/images/logo-light.svg" alt="" height={22} />
               </span>
               <span className="logo-lg">
                 <img src="/assets/images/logo-light.png" alt="" height={19} />
               </span>
-            </a>
+            </a> */}
           </div>
           <button
             type="button"
@@ -36,7 +36,7 @@ export default function Topbar() {
           </button>
           {/* App Search*/}
 
-          <div className="dropdown dropdown-mega d-none d-lg-block ms-2">
+          {/* <div className="dropdown dropdown-mega d-none d-lg-block ms-2">
             <button
               type="button"
               className="btn header-item waves-effect"
@@ -231,343 +231,9 @@ export default function Topbar() {
                 </div>
               </div>
             </div>
-          </div>
+          </div> */}
         </div>
         <div className="d-flex">
-          <div className="dropdown d-inline-block d-lg-none ms-2">
-            <button
-              type="button"
-              className="btn header-item noti-icon waves-effect"
-              id="page-header-search-dropdown"
-              data-bs-toggle="dropdown"
-              aria-haspopup="true"
-              aria-expanded="false"
-            >
-              <i className="mdi mdi-magnify" />
-            </button>
-            <div
-              className="dropdown-menu dropdown-menu-lg dropdown-menu-end p-0"
-              aria-labelledby="page-header-search-dropdown"
-            >
-              <form className="p-3">
-                <div className="form-group m-0">
-                  <div className="input-group">
-                    <input
-                      type="text"
-                      className="form-control"
-                      placeholder="Search ..."
-                      aria-label="Recipient's username"
-                    />
-                    <div className="input-group-append">
-                      <button className="btn btn-primary" type="submit">
-                        <i className="mdi mdi-magnify" />
-                      </button>
-                    </div>
-                  </div>
-                </div>
-              </form>
-            </div>
-          </div>
-          <div className="dropdown d-inline-block">
-            <button
-              type="button"
-              className="btn header-item waves-effect"
-              data-bs-toggle="dropdown"
-              aria-haspopup="true"
-              aria-expanded="false"
-            >
-              <img
-                id="header-lang-img"
-                src="/assets/images/flags/us.jpg"
-                alt="Header Language"
-                height={16}
-              />
-            </button>
-            <div className="dropdown-menu dropdown-menu-end">
-              {/* item*/}
-              <a
-                href="#"
-                className="dropdown-item notify-item language"
-                data-lang="en"
-              >
-                <img
-                  src="/assets/images/flags/us.jpg"
-                  alt="user-image"
-                  className="me-1"
-                  height={12}
-                />{" "}
-                <span className="align-middle">English</span>
-              </a>
-              {/* item*/}
-              <a
-                href="#"
-                className="dropdown-item notify-item language"
-                data-lang="sp"
-              >
-                <img
-                  src="/assets/images/flags/spain.jpg"
-                  alt="user-image"
-                  className="me-1"
-                  height={12}
-                />{" "}
-                <span className="align-middle">Spanish</span>
-              </a>
-              {/* item*/}
-              <a
-                href="#"
-                className="dropdown-item notify-item language"
-                data-lang="gr"
-              >
-                <img
-                  src="/assets/images/flags/germany.jpg"
-                  alt="user-image"
-                  className="me-1"
-                  height={12}
-                />{" "}
-                <span className="align-middle">German</span>
-              </a>
-              {/* item*/}
-              <a
-                href="#"
-                className="dropdown-item notify-item language"
-                data-lang="it"
-              >
-                <img
-                  src="/assets/images/flags/italy.jpg"
-                  alt="user-image"
-                  className="me-1"
-                  height={12}
-                />{" "}
-                <span className="align-middle">Italian</span>
-              </a>
-              {/* item*/}
-              <a
-                href="#"
-                className="dropdown-item notify-item language"
-                data-lang="ru"
-              >
-                <img
-                  src="/assets/images/flags/russia.jpg"
-                  alt="user-image"
-                  className="me-1"
-                  height={12}
-                />{" "}
-                <span className="align-middle">Russian</span>
-              </a>
-            </div>
-          </div>
-          <div className="dropdown d-none d-lg-inline-block ms-1">
-            <button
-              type="button"
-              className="btn header-item noti-icon waves-effect"
-              data-bs-toggle="dropdown"
-              aria-haspopup="true"
-              aria-expanded="false"
-            >
-              <i className="bx bx-customize" />
-            </button>
-            <div className="dropdown-menu dropdown-menu-lg dropdown-menu-end">
-              <div className="px-lg-2">
-                <div className="row g-0">
-                  <div className="col">
-                    <a className="dropdown-icon-item" href="#">
-                      <img
-                        src="/assets/images/brands/github.png"
-                        alt="Github"
-                      />
-                      <span>GitHub</span>
-                    </a>
-                  </div>
-                  <div className="col">
-                    <a className="dropdown-icon-item" href="#">
-                      <img
-                        src="/assets/images/brands/bitbucket.png"
-                        alt="bitbucket"
-                      />
-                      <span>Bitbucket</span>
-                    </a>
-                  </div>
-                  <div className="col">
-                    <a className="dropdown-icon-item" href="#">
-                      <img
-                        src="/assets/images/brands/dribbble.png"
-                        alt="dribbble"
-                      />
-                      <span>Dribbble</span>
-                    </a>
-                  </div>
-                </div>
-                <div className="row g-0">
-                  <div className="col">
-                    <a className="dropdown-icon-item" href="#">
-                      <img
-                        src="/assets/images/brands/dropbox.png"
-                        alt="dropbox"
-                      />
-                      <span>Dropbox</span>
-                    </a>
-                  </div>
-                  <div className="col">
-                    <a className="dropdown-icon-item" href="#">
-                      <img
-                        src="/assets/images/brands/mail_chimp.png"
-                        alt="mail_chimp"
-                      />
-                      <span>Mail Chimp</span>
-                    </a>
-                  </div>
-                  <div className="col">
-                    <a className="dropdown-icon-item" href="#">
-                      <img src="/assets/images/brands/slack.png" alt="slack" />
-                      <span>Slack</span>
-                    </a>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="dropdown d-none d-lg-inline-block ms-1">
-            <button
-              type="button"
-              className="btn header-item noti-icon waves-effect"
-              data-bs-toggle="fullscreen"
-            >
-              <i className="bx bx-fullscreen" />
-            </button>
-          </div>
-          <div className="dropdown d-inline-block">
-            <button
-              type="button"
-              className="btn header-item noti-icon waves-effect"
-              id="page-header-notifications-dropdown"
-              data-bs-toggle="dropdown"
-              aria-haspopup="true"
-              aria-expanded="false"
-            >
-              <i className="bx bx-bell bx-tada" />
-              <span className="badge bg-danger rounded-pill">3</span>
-            </button>
-            <div
-              className="dropdown-menu dropdown-menu-lg dropdown-menu-end p-0"
-              aria-labelledby="page-header-notifications-dropdown"
-            >
-              <div className="p-3">
-                <div className="row align-items-center">
-                  <div className="col">
-                    <h6 className="m-0" key="t-notifications">
-                      {" "}
-                      Notifications{" "}
-                    </h6>
-                  </div>
-                  <div className="col-auto">
-                    <a href="#!" className="small" key="t-view-all">
-                      {" "}
-                      View All
-                    </a>
-                  </div>
-                </div>
-              </div>
-              <div data-simplebar="" style={{ maxHeight: 230 }}>
-                <a href="#" className="text-reset notification-item">
-                  <div className="d-flex">
-                    <div className="avatar-xs me-3">
-                      <span className="avatar-title bg-primary rounded-circle font-size-16">
-                        <i className="bx bx-cart" />
-                      </span>
-                    </div>
-                    <div className="flex-grow-1">
-                      <h6 className="mb-1" key="t-your-order">
-                        Your order is placed
-                      </h6>
-                      <div className="font-size-12 text-muted">
-                        <p className="mb-1" key="t-grammer">
-                          If several languages coalesce the grammar
-                        </p>
-                        <p className="mb-0">
-                          <i className="mdi mdi-clock-outline" />{" "}
-                          <span key="t-min-ago">3 min ago</span>
-                        </p>
-                      </div>
-                    </div>
-                  </div>
-                </a>
-                <a href="#" className="text-reset notification-item">
-                  <div className="d-flex">
-                    <img
-                      src="/assets/images/users/avatar-3.jpg"
-                      className="me-3 rounded-circle avatar-xs"
-                      alt="user-pic"
-                    />
-                    <div className="flex-grow-1">
-                      <h6 className="mb-1">James Lemire</h6>
-                      <div className="font-size-12 text-muted">
-                        <p className="mb-1" key="t-simplified">
-                          It will seem like simplified English.
-                        </p>
-                        <p className="mb-0">
-                          <i className="mdi mdi-clock-outline" />{" "}
-                          <span key="t-hours-ago">1 hours ago</span>
-                        </p>
-                      </div>
-                    </div>
-                  </div>
-                </a>
-                <a href="#" className="text-reset notification-item">
-                  <div className="d-flex">
-                    <div className="avatar-xs me-3">
-                      <span className="avatar-title bg-success rounded-circle font-size-16">
-                        <i className="bx bx-badge-check" />
-                      </span>
-                    </div>
-                    <div className="flex-grow-1">
-                      <h6 className="mb-1" key="t-shipped">
-                        Your item is shipped
-                      </h6>
-                      <div className="font-size-12 text-muted">
-                        <p className="mb-1" key="t-grammer">
-                          If several languages coalesce the grammar
-                        </p>
-                        <p className="mb-0">
-                          <i className="mdi mdi-clock-outline" />{" "}
-                          <span key="t-min-ago">3 min ago</span>
-                        </p>
-                      </div>
-                    </div>
-                  </div>
-                </a>
-                <a href="#" className="text-reset notification-item">
-                  <div className="d-flex">
-                    <img
-                      src="/assets/images/users/avatar-4.jpg"
-                      className="me-3 rounded-circle avatar-xs"
-                      alt="user-pic"
-                    />
-                    <div className="flex-grow-1">
-                      <h6 className="mb-1">Salena Layfield</h6>
-                      <div className="font-size-12 text-muted">
-                        <p className="mb-1" key="t-occidental">
-                          As a skeptical Cambridge friend of mine occidental.
-                        </p>
-                        <p className="mb-0">
-                          <i className="mdi mdi-clock-outline" />{" "}
-                          <span key="t-hours-ago">1 hours ago</span>
-                        </p>
-                      </div>
-                    </div>
-                  </div>
-                </a>
-              </div>
-              <div className="p-2 border-top d-grid">
-                <a
-                  className="btn btn-sm btn-link font-size-14 text-center"
-                  href="#"
-                >
-                  <i className="mdi mdi-arrow-right-circle me-1" />{" "}
-                  <span key="t-view-more">View More..</span>
-                </a>
-              </div>
-            </div>
-          </div>
           <div className="dropdown d-inline-block">
             <button
               type="button"
@@ -612,14 +278,6 @@ export default function Topbar() {
                 <span key="t-logout">Logout</span>
               </a>
             </div>
-          </div>
-          <div className="dropdown d-inline-block">
-            <button
-              type="button"
-              className="btn header-item noti-icon right-bar-toggle waves-effect"
-            >
-              <i className="bx bx-cog bx-spin" />
-            </button>
           </div>
         </div>
       </div>
